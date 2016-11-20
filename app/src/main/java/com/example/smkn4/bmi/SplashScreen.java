e76846486cf07003d3c88b2f20373fc4368d4d0d
misdan.wijaya07 <misdan.wijaya07@gmail.com>
2016-11-20 11:32:45 +0700

    SecondCommit

--- app/src/main/java/com/example/smkn4/bmi/SplashScreen.java
+++ app/src/main/java/com/example/smkn4/bmi/SplashScreen.java
@@ -4,9 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-/**
- * Created by vamsikrishna on 12-Feb-15.
- */
 public class SplashScreen extends Activity {
 
     @Override
